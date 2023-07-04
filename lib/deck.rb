f2d856008fa7bb533085498834d29c803147c1a0
Reed Hillmar <reedhillmar@gmail.com>
2023-07-03 23:59:10 -0600

    Add methods and test code through iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,28 @@ class Deck
         cards.fetch(index_location).rank
     end
 
+    def high_ranking_cards
+        # high_ranking_cards = []
+        
+        # cards.each do |card|
+        #     high_ranking_cards << card if card.rank >= 11
+        # end
+
+        # high_ranking_cards
+
+        cards.select {|card| card.rank >= 11}
+    end
+
+    def percent_high_ranking
+        ((self.high_ranking_cards.length.to_f / cards.length.to_f)*100).round(2)
+    end
+
+    def remove_card
+        cards.shift
+    end
+
+    def add_card(card)
+        cards << card
+    end
+
 end
