23b87379200d4f624d55999ac8bcafb06e1c23fa
Reed Hillmar <reedhillmar@gmail.com>
2023-07-03 19:39:11 -0600

    Initialize Deck class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/card'
+require './lib/deck'
 
 RSpec.describe Card do
   it "exists" do
@@ -14,5 +15,19 @@ RSpec.describe Card do
     expect(card.suit).to eq(:diamond)
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
+  end
+end
+
+RSpec.describe Deck do
+  it "has a readable attribute" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
   end
 end
